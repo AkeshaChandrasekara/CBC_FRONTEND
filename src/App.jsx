413d278a4e0109952f91a15789e08426cfc87571
Akesha <akeshanawanjali23@gmail.com>
2025-06-23 21:10:21 +0530

    login completed✔️

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LoginPage from './pages/loginPage.jsx'
 import HomePage from './pages/HomePage.jsx'
 import SignUpPage from './pages/signUpPage.jsx' 
 import AdminHomePage from './pages/adminHomePage.jsx'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,11 +13,12 @@ function App() {
   return (
     <>
       <BrowserRouter>
+      <Toaster/>
         <Routes>
           <Route path="/" element={<LoginPage/>}/>
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/home" element={<HomePage/>}/>
-          <Route path="/admin/*" element={<AdminHomePage/>}/>
+          <Route path="/admin" element={<AdminHomePage/>}/>
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,19 +1,59 @@
+import axios from "axios";
+import { useState } from 'react'; 
+import toast from "react-hot-toast";
+
 export default function LoginPage() {
+
+  function login(){
+    axios.post("http://localhost:5000/api/users/login", {
+      email: email,
+      password: password
+    
+    })
+      .then((response) => {
+        console.log(response.data);
+        if(response.data.user==null){
+          toast.error(response.data.message)
+          return
+        }
+        toast.success(response.data.message)
+        localStorage.setItem("token", response.data.token);
+        if(response.data.user.type == "admin"){
+          window.location.href = "/admin"
+        }else{
+          window.location.href = "/home"
+        }
+
+
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+  }
+
+  const [email, setEmail] = useState("your email")
+  const [password, setPassword] = useState('')
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-8 rounded shadow-md w-80">
         <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
         <input 
           type="text" 
-          placeholder="Username" 
+          placeholder="Email" 
           className="w-full p-2 mb-4 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+         defaultValue={email}
+         onChange={(e) => setEmail(e.target.value)}
+
         />
         <input 
           type="password" 
           placeholder="Password" 
           className="w-full p-2 mb-6 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+         defaultValue={password}
+         onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="w-full bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded transition-colors">
+        <button className="w-full bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded transition-colors "onClick={login}>
           Login
         </button>
       </div>
