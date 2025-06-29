e585e1a30719d38f84d94079ae96f5f3eab36886
Akesha <akeshanawanjali23@gmail.com>
2025-06-29 11:14:36 +0530

    update prdocu card📦

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 
+
+
 export default function ProductCard(props) {
   const product = props.product;
   const isDiscounted = product.lastPrice < product.price;
   const discountPercentage = isDiscounted 
     ? Math.round(((product.price - product.lastPrice) / product.price) * 100)
     : 0;
- 
+   
 
   return (
     <div className="group relative bg-white rounded-lg shadow-md hover:shadow-lg transition-all duration-300 overflow-hidden flex flex-col h-full border border-gray-200">
@@ -76,6 +78,7 @@ export default function ProductCard(props) {
             View Details
           </Link>
           <button 
+       
             className="text-sm font-medium bg-yellow-500 text-white px-3 py-1 rounded hover:bg-primary-dark transition-colors"
           >
             Add to Cart
