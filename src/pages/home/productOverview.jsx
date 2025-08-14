df04e6c1ee75afd9bde44d8039e8cc8895dbe05a
Akesha <akeshanawanjali23@gmail.com>
2025-08-14 15:51:14 +0530

    update design in the product overview page's review section🔸

--- src/pages/home/productOverview.jsx
+++ src/pages/home/productOverview.jsx
@@ -263,7 +263,7 @@ export default function ProductOverview() {
             </div>
           </div>
 
-          <div className="mt-12 bg-white rounded-xl shadow-sm overflow-hidden border border-gray-200 p-6">
+          <div className="mt-2 bg-white rounded-xl shadow-sm overflow-hidden border border-gray-200 p-6">
             <div className="flex justify-between items-center mb-6">
               <h2 className="text-2xl font-bold text-gray-900">Customer Reviews</h2>
               {getCurrentUserEmail() && (
